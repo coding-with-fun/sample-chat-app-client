1d4ea04a8d8b305e82bde04e1d619eb5366b298f
Harrsh Patel <me.harrsh.patel@gmail.com>
2021-12-10 20:23:54 +0530

    Added chat socket connection

--- src/containers/Chat.container.js
+++ src/containers/Chat.container.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+import { useLocation } from "react-router-dom";
 
 const ChatContainer = () => {
+    const location = useLocation();
+
+    const [name, setName] = useState("");
+    const [roomName, setRoomName] = useState("");
+    const [socket, setSocket] = useState();
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        setName(name);
+        setRoomName(room);
+
+        const socketInit = io("localhost:5050");
+        console.log(socketInit);
+        setSocket(socketInit);
+
+        socketInit.emit(
+            "join",
+            {
+                name,
+                room,
+            },
+            ({ error }) => {
+                console.log(error);
+            }
+        );
+    }, [location]);
+
     return (
         <div>
             <h1>Chat</h1>
